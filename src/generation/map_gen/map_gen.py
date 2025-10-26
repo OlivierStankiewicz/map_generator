d7ef51029068e2502750ba66acf53e0696871f85
kempix360 <kempix360@gmail.com>
2025-10-26 13:52:24 +0100

    merged with object generation and map gen bugs fixed

--- src/generation/map_gen/map_gen.py
+++ src/generation/map_gen/map_gen.py
@@ -1,15 +1,13 @@
-import random
 from typing import Dict
 
-from classes.map import Map
+from classes.Map import Map
 from classes.tile.Tile import TerrainType
 from generation.basic_info_gen import generate_basic_info
 from generation.player_gen.player_gen import generate_player
 from generation.additional_info_gen.additional_info_gen import generate_additional_info
 from generation.tile_gen.tile_gen import generate_tile, generate_specific_terrain_and_sprite, get_terrain_type_sprite_range
 from generation.objects_template_gen import generate_objects_template
-from generation.pcg_algorithms.perlin import perlin
-from generation.pcg_algorithms.voronoi import voronoi
+from generation.pcg_algorithms.voronoi import VoronoiMapGenerator
 from generation.map_gen.utils import upscale_map, smooth_map
 
 def generate_base_map() -> Map:
@@ -89,65 +87,6 @@ def generate_sprite_comparison_map(terrain_type: TerrainType, compared_range: li
         padding= [0] * 124
     )
 
-def generate_perlin_noise_map(width=72, height=72, scale=10.0, octaves=2, persistence=0.5, lacunarity=2.0, seed=None) -> Map:
-    """
-    Generate a Map using Perlin noise to assign terrain types.
-    """
-    if seed is not None:
-        random.seed(seed)
-
-    tiles = []
-    terrain_types = list(TerrainType)
-    num_types = len(terrain_types)
-
-    small_width, small_height = width // 2, height // 2
-    terrain_map = [[None for _ in range(small_width)] for _ in range(small_height)]
-
-    for y in range(small_height):
-        for x in range(small_width):
-            amplitude = 1.0
-            frequency = 1.0
-            value = 0.0
-            max_amp = 0.0
-
-            for _ in range(octaves):
-                sample_x = (x / scale) * frequency
-                sample_y = (y / scale) * frequency
-                n = perlin(sample_x, sample_y)
-
-                value += n * amplitude
-                max_amp += amplitude
-                amplitude *= persistence
-                frequency *= lacunarity
-
-            normalized = (value / max_amp + 1) / 2  # map [-1,1] -> [0,1]
-            terrain_index = round(normalized * (num_types - 1))
-            terrain_map[y][x] = terrain_types[terrain_index]
-
-    # upscale map
-    upscaled_terrain_map = upscale_map(terrain_map=terrain_map)
-    for y in range(height):
-        for x in range(width):
-            terrain_type = upscaled_terrain_map[y][x]
-            tiles.append(generate_specific_terrain_and_sprite(terrain_type, 1))
-
-    # underground
-    for y in range(height):
-        for x in range(width):
-            tiles.append(generate_specific_terrain_and_sprite(TerrainType.ROCK, 1))
-
-    return Map(
-        format=28,
-        basic_info=generate_basic_info(),
-        players=[generate_player() for _ in range(8)],
-        additional_info=generate_additional_info(),
-        tiles=tiles,
-        objects_templates=[generate_objects_template()],
-        objects=[],
-        global_events=[],
-        padding=[0] * 124
-    )
-
 def generate_voronoi_map(
     terrain_values: Dict[TerrainType, int] = {
         TerrainType.WATER: 1,
@@ -161,7 +100,8 @@ def generate_voronoi_map(
     Generate a map using Voronoi regions to assign terrain types.
     """
     tiles = []
-    terrain_map = voronoi(terrain_weights=terrain_values, height=height//2, width=width//2)
+    generator = VoronoiMapGenerator(height=height//2, width=width//2, terrain_weights=terrain_values)
+    terrain_map = generator.generate_map()
 
     # upscale map
     terrain_map = upscale_map(terrain_map=terrain_map)
