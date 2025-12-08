c221bd2671d5a0c108075a3475df041000b64752
piotreuszxx <130872389+piotreuszxx@users.noreply.github.com>
2025-12-08 03:07:33 +0100

    remove GUI things

--- src/GUI/functions/filter_none_values.py
+++
@@ -1,8 +0,0 @@
-def filter_none_values(obj):
-    """Recursively remove keys with None values from dictionaries or lists."""
-    if isinstance(obj, dict):
-        return {k: filter_none_values(v) for k, v in obj.items() if v is not None}
-    elif isinstance(obj, list):
-        return [filter_none_values(item) for item in obj if item is not None]
-    else:
-        return obj
